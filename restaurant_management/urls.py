e9b7d6ee364b0e58c383866cc8b13a973161d536
Tobiloba Oyeniyi <oyeniyitobiloba12@gmail.com>
2026-09-13 09:25:42 +0100

    Add restaurant menu images

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from accounts.views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("",home, name="home"),
@@ -32,7 +33,12 @@ urlpatterns = [
     path("managers/", include("managers.urls")),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    path(
+        "media/<path:path>",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
