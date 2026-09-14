29fa777ce8bd5a7d50f399cd311325cda2fc5c11
Tobiloba Oyeniyi <oyeniyitobiloba12@gmail.com>
2026-09-14 21:48:40 +0100

    Add Cloudinary image storage

--- menu/models.py
+++ menu/models.py
@@ -1,22 +1,49 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
 class Category(models.Model):
+
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
 
-class MenuItem(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu/', blank=True, null=True)
-    is_available = models.BooleanField(default=True)
 
-   
+class MenuItem(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='menu_items'
+    )
+
+    name = models.CharField(max_length=100)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    image = CloudinaryField(
+        'image',
+        blank=True,
+        null=True
+    )
+
+    is_available = models.BooleanField(
+        default=True
+    )
 
     def __str__(self):
         return self.name
